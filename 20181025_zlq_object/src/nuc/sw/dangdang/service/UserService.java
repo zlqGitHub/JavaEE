72d34c8f86749e0fe28bfa1e5337d9da9029650b
zlqGitHub <1509874021@qq.com>
2018-12-07 14:29:54 +0800

    service服务类

--- 20181025_zlq_object/src/nuc/sw/dangdang/service/UserService.java
+++ 20181025_zlq_object/src/nuc/sw/dangdang/service/UserService.java
@@ -3,17 +3,17 @@ package nuc.sw.dangdang.service;
 import java.util.List;
 
 import nuc.sw.dangdang.dao.UserDao;
-import nuc.sw.dangdang.entity.User;
+import nuc.sw.dangdang.entity.Student;
 
 public class UserService {
 	UserDao uDao = new UserDao();
 	//注册添加一个用户
-	public void insertUser(User u) {
+	public void insertUser(Student u) {
 		uDao.insertUser(u);
 	}
 	
 	//获取所有用户
-	public List<User> listUsers(){
+	public List<Student> listUsers(){
 		return uDao.listUsers();
 	}
 
